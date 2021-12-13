a977259447109dd3ecea44ca8d1c43d9375c36c2
Josselin Lecocq <josselin@lecocq.io>
2021-12-14 00:18:14 +0100

    Day 13 refactoring

--- 2021/day13.php
+++ 2021/day13.php
@@ -4,36 +4,23 @@
 
 
 $input = file_get_contents('input13.txt');
-$input = explode("\n\n", trim(str_replace('fold along ', '', $input)));
+$input = explode("\n\n", trim(str_replace(['fold along ', 'x', 'y'], ['', '0', '1'], $input)));
 $dots = array_map( function($a) { return explode(',', $a); }, explode("\n", $input[0]));
 $folds = array_map( function($a) { return explode('=', $a); }, explode("\n", $input[1]));
 
 
 foreach ($folds as $foldid => $fold) {
-
-	if($fold[0] == 'x') { // fold along x
-		$xmax = max(array_column($dots, 0));
-		foreach($dots as $dotid => $dot) {
-			if($fold[1] >= $xmax/2) {
-				$dots[$dotid][0] = ($dot[0] > $fold[1]) ? $fold[1]-($dot[0]-$fold[1]) : $dot[0];
-			}
-			else {
-				$dots[$dotid][0] = ($dot[0] < $fold[1]) ? $fold[1]-($dot[0]-$fold[1]) : $dot[0];
-			}
+	
+	$max = max(array_column($dots, $fold[0]));
+	foreach($dots as $dotid => $dot) {
+		if($fold[1] >= $max/2) {
+			$dots[$dotid][$fold[0]] = ($dot[$fold[0]] > $fold[1]) ? $fold[1]-($dot[$fold[0]]-$fold[1]) : $dot[$fold[0]];
+		}
+		else {
+			$dots[$dotid][$fold[0]] = ($dot[$fold[0]] < $fold[1]) ? $fold[1]-($dot[$fold[0]]-$fold[1]) : $dot[$fold[0]];
 		}
 	}
-	else { // fold along y
-		$ymax = max(array_column($dots, 1));
-		foreach($dots as $dotid => $dot) {
-			if($fold[1] >= $ymax/2) {
-				$dots[$dotid][1] = ($dot[1] > $fold[1]) ? $fold[1]-($dot[1]-$fold[1]) : $dot[1];
-			}
-			else {
-				$dots[$dotid][1] = ($dot[1] < $fold[1]) ? $fold[1]-($dot[1]-$fold[1]) : $dot[1];
-			}
-		}
-	}
-
+	
 	$dots = array_unique($dots, SORT_REGULAR);
 
 	if($foldid == 0) echo 'Part 1 - First fold only : '.count($dots)."\n";
